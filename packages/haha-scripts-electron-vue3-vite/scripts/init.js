5cbb63a8c27da5d325f162ccf63a7186129bd784
chenwj <731028571@qq.com>
2021-11-23 11:52:59 +0800

    chore: update dependencies

--- packages/haha-scripts-electron-vue3-vite/scripts/init.js
+++ packages/haha-scripts-electron-vue3-vite/scripts/init.js
@@ -1,20 +1,22 @@
 const spawn = require('cross-spawn');
 const packageJson = require('../package.json')
+const fs = require('fs-extra')
+const path = require('path')
 
 module.exports = function (
     appPath,
     appName,
     originalDirectory) {
     console.log('hi, there was haha-scripts')
-    console.log('template was ', packageJson.templateOptions[0]) // 这里要改成命令行选择
-    // todo: npm 下载 template . copy. package.json等细节 删除. 
+    const tpl = packageJson.templateOptions[0] // 这里要改成命令行选择
+    console.log('template was ', tpl)
 
-    // todo: 写bin入口, 写start, build的脚本
-    // todo: 写template, 并引入这个包, 看看是不是能执行
-
-    spawn('yarnpkg', ['add', packageJson.templateOptions[0], '--cwd', appPath], { stdio: 'inherit' }).on('close', code => {
+    spawn('yarnpkg', ['add', tpl, '--cwd', appPath], { stdio: 'inherit' }).on('close', code => {
         if (code === 0) {
             console.log('haha-scripts was installed successfully!')
+            fs.copySync(path.join(__dirname, 'node_modules', tpl, 'template'), appPath)
+            process.chdir(appPath)
+            spawn('yarn', ['install'], { stdio: 'inherit' })
         } else {
             console.log('haha-scripts was failed to install!')
         }
